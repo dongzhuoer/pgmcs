cbab874a5503e654896af20a2478865f087c5f75
Travis CI User <travis@example.org>
2019-04-18 10:30:59 +0000

    [skip ci] build pkgdown at 2019-04-18 10:30:59

--- inst/deploy.R
+++
@@ -1,2 +0,0 @@
-system.file('shiny/population-genetics-monte-carlo-simulation', package = 'pgmcs') %>% rsconnect::deployApp()
-system.file('shiny/population-genetics-monte-carlo-simulation2', package = 'pgmcs') %>% rsconnect::deployApp()
